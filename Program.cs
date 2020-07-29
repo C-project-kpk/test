842a581e8d6d5bd2afb79cdee8db5d75e44883e0
SOW_ZOM <o26032002z@gmail.com>
2020-07-30 00:33:45 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ffff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
